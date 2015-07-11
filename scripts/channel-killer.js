e0c2850dac554e25a2e6e06f9654712cb8a8e2fc
tyage <namatyage@gmail.com>
2015-07-11 17:47:00 +0900

    send archive api

--- scripts/channel-killer.js
+++ scripts/channel-killer.js
@@ -29,11 +29,10 @@ var isChannelUnused = function(channel) {
   var lastUpdate = new Date(channel.latest.ts * 1000);
   return (new Date() - lastUpdate) > threshold;
 };
-var getUnusedChannels = function(callback) {
-  var unusedChannels = Object.keys(client.channels).filter(function(id) {
+var getUnusedChannels = function() {
+  return Object.keys(client.channels).filter(function(id) {
     return isChannelUnused(client.channels[id]);
   });
-  callback(unusedChannels);
 };
 
 client.login();
@@ -61,24 +60,28 @@ client.on('raw_message', function(message) {
 });
 
 module.exports = function(robot) {
+  var formatChannels = function(channels) {
+    return channels.map(function(channel) {
+      return "#" + channel;
+    }).join(' ');
+  };
+
+  // say the list of channels to be killed
   robot.respond(/list/i, function(res) {
-    // say the list of channels to be killed
     res.reply("Following channels are not used:");
-    getUnusedChannels(function(unusedChannels) {
-      var list = unusedChannels.map(function(id) {
-        return "#" + client.channels[id].name;
-      });
-      res.reply(list.join(", "));
+    var channels = getUnusedChannels().map(function(id) {
+      return client.channels[id].name;
     });
+    res.reply(formatChannels(channels));
   });
+
+  // archive the channel
   robot.respond(/kill/i, function(res) {
-    // archive the channel
     res.reply("Following channels will be archived:");
-    getUnusedChannels(function(unusedChannels) {
-      var list = unusedChannels.map(function(id) {
-        return "#" + client.channels[id].name;
-      });
-      res.reply(list.join(", "));
+    var channels = getUnusedChannels().map(function(id) {
+      client._apiCall('channels.archive', { channel: id });
+      return client.channels[id].name;
     });
+    res.reply(formatChannels(channels));
   });
 };
