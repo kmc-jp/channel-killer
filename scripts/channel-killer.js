f41ba10da74759183ed7a0f5d1d2aa42bcfd3773
tyage <namatyage@gmail.com>
2015-07-11 17:38:46 +0900

    rtm対応

--- scripts/channel-killer.js
+++ scripts/channel-killer.js
@@ -8,11 +8,14 @@ var options = {
   autoMark: true
 };
 var client = new SlackClient(options.token, options.autoReconnect, options.autoMark);
-client.login();
-client.on('loggedIn', function(self, team) {
-  console.log('logged in');
-});
 
+var joinAllChannels = function() {
+  Object.keys(client.channels).filter(function(id) {
+    if (!client.channels[id].is_member) {
+      client.joinChannel(client.channels[id].name);
+    }
+  });
+};
 var isChannelUnused = function(channel) {
   if (channel.is_archived) {
     return false;
@@ -23,7 +26,6 @@ var isChannelUnused = function(channel) {
     return false;
   }
 
-  console.log(channel);
   var lastUpdate = new Date(channel.latest.ts * 1000);
   return (new Date() - lastUpdate) > threshold;
 };
@@ -33,6 +35,30 @@ var getUnusedChannels = function(callback) {
   });
   callback(unusedChannels);
 };
+
+client.login();
+client.on('loggedIn', function(self, team) {
+  console.log('logged in');
+  joinAllChannels();
+});
+client.on('raw_message', function(message) {
+  switch (message.type) {
+    case 'message':
+      client.channels[message.channel].latest = message;
+      break;
+    case 'channel_left':
+    case 'channel_unarchive':
+      var channel = client.channels[message.channel];
+      if (channel) {
+        client.joinChannel(channel.name);
+      }
+      break;
+    case 'channel_created':
+      client.joinChannel(message.channel.name);
+      break;
+    default:
+  }
+});
 
 module.exports = function(robot) {
   robot.respond(/list/i, function(res) {
