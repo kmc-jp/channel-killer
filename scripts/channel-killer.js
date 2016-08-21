0b11482180001d8a8bfcf29b39a6ba9a70f1ca8a
tyage <namatyage@gmail.com>
2016-08-21 23:58:37 +0900

    update slack-client -> @slack/client

--- scripts/channel-killer.js
+++ scripts/channel-killer.js
@@ -11,29 +11,60 @@
 // Author:
 //   tyage <namatyage@gmail.com>
 
-var SlackClient = require('slack-client');
+var RtmClient = require('@slack/client').RtmClient;
+var WebClient = require('@slack/client').WebClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-var options = {
-  token: process.env.HUBOT_SLACK_TOKEN,
-  autoReconnect: true,
-  autoMark: true
+var token = process.env.HUBOT_SLACK_TOKEN;
+var web = new WebClient(token);
+var rtm = new RtmClient(token);
+
+var channels = [];
+
+var getChannelIndex = function(id) {
+  var result;
+  channels.forEach(function(channel, i) {
+    if (channel.id === id) {
+      result = i;
+    }
+  });
+  return result;
 };
-var client = new SlackClient(options.token, options.autoReconnect, options.autoMark);
-
-var joinAllChannels = function() {
-  Object.keys(client.channels).filter(function(id) {
-    if (!client.channels[id].is_member) {
-      client.joinChannel(client.channels[id].name);
+var getChannel = function(id) {
+  var index = getChannelIndex(id);
+  return channels[index];
+};
+var updateChannelInfo = function(id) {
+  web.channels.info(id).then(function(info) {
+    var index = getChannelIndex(id);
+    if (index) {
+      channels[index].latest = info.channel.latest;
     }
   });
 };
+var joinChannel = function(channel) {
+  web.channels.join(channel.name);
+  updateChannelInfo(channel.id);
+};
+var joinAllChannels = function() {
+  web.channels.list().then(function(info) {
+    info.channels.forEach(function(channel) {
+      if (!channel.is_member) {
+        joinChannel(channel);
+      }
+    });
+  });
+};
+
 var isChannelUnused = function(channel, threshold) {
   if (channel.is_archived) {
     return false;
   }
-  // channel.latestはjoinしているチャンネルしか取得できない
+
+  // channel.latestはjoinしているチャンネルしか取得できないのでjoinする
   if (!channel.latest) {
-    client.joinChannel(channel.name);
+    joinChannel(channel);
     return false;
   }
 
@@ -41,41 +72,43 @@ var isChannelUnused = function(channel, threshold) {
   return (new Date() - lastUpdate) > threshold;
 };
 var getUnusedChannels = function(threshold) {
-  return Object.keys(client.channels).filter(function(id) {
-    return isChannelUnused(client.channels[id], threshold);
+  return channels.filter(function(channel) {
+    return isChannelUnused(channel, threshold);
   });
 };
 
-client.login();
-client.on('loggedIn', function(self, team) {
+rtm.start();
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
   console.log('logged in');
+  channels = rtmStartData.channels;
   joinAllChannels();
 });
-client.on('raw_message', function(message) {
-  switch (message.type) {
-    case 'message':
-      if (client.channels[message.channel]) {
-        client.channels[message.channel].latest = message;
-      }
-      break;
-    case 'channel_left':
-    case 'channel_unarchive':
-      var channel = client.channels[message.channel];
-      if (channel) {
-        client.joinChannel(channel.name);
-      }
-      break;
-    case 'channel_created':
-      client.joinChannel(message.channel.name);
-      break;
-    default:
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+  var channel = getChannel(message.channel);
+  if (channel) {
+    channel.latest = message;
   }
+});
+rtm.on(RTM_EVENTS.CHANNEL_LEFT, function(message) {
+  var channel = getChannel(message.channel);
+  if (channel) {
+    joinChannel(channel);
+  }
+});
+rtm.on(RTM_EVENTS.CHANNEL_UNARCHIVE, function(message) {
+  var channel = getChannel(message.channel);
+  if (channel) {
+    joinChannel(channel);
+  }
+});
+rtm.on(RTM_EVENTS.CHANNEL_CREATED, function(message) {
+  joinChannel(message.channel);
 });
 
 module.exports = function(robot) {
   var formatChannels = function(channels) {
     return channels.map(function(channel) {
-      return "#" + channel;
+      return "#" + channel.name;
     }).join(' ');
   };
 
@@ -85,20 +118,19 @@ module.exports = function(robot) {
     var threshold = days * 24 * 60 * 60 * 1000;
     res.reply('Following channels are not used for ' + days + 'days:');
 
-    var channels = getUnusedChannels(threshold).map(function(id) {
-      return client.channels[id].name;
-    });
+    var channels = getUnusedChannels(threshold);
     res.reply(formatChannels(channels));
   });
 
   // archive the channel
   robot.respond(/kill ([0-9]+)days/i, function(res) {
-    var threshold = parseInt(res.match[1]) * 24 * 60 * 60 * 1000;
+    var days = parseInt(res.match[1]);
+    var threshold = days * 24 * 60 * 60 * 1000;
     res.reply('Following channels will be archived:');
 
-    var channels = getUnusedChannels(threshold).map(function(id) {
-      client._apiCall('channels.archive', { channel: id });
-      return client.channels[id].name;
+    var channels = getUnusedChannels(threshold);
+    channels.forEach(function(channel) {
+      web.channels.archive(channel.id);
     });
     res.reply(formatChannels(channels));
   });
